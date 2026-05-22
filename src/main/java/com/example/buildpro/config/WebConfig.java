7422f14c7baea9d453700012587e29fea9c9077e
Mohankanth Maddina <mohankanth1784@gmail.com>
2026-05-22 21:56:49 +0530

    domain issue resolver 1

--- src/main/java/com/example/buildpro/config/WebConfig.java
+++ src/main/java/com/example/buildpro/config/WebConfig.java
@@ -10,8 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                // .allowedOriginPatterns("http://localhost:3000", "http://127.0.0.1:5500")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://buildersmartenterprices.in",
+                        "https://buildersmartenterprices.in"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
